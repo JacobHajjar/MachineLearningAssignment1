392296ac1d3294b03f1cf9805f0bb79b7f2a4566
Jacob Hajjar <jacob.hajjar1@gmail.com>
2022-09-23 12:18:44 -0700

    populated the data for regression

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import pandas as pd
+import numpy
 from sklearn import linear_model
 
 ''' develop the best predictive model based on the chemical engineering dataset'''
@@ -11,7 +12,10 @@ __maintainer__ = 'jacobhajjar'
 
 def main():
     '''the main function'''
-    data_file = pd.read_csv("Data1.csv")
+    data_frame = pd.read_csv("Data1.csv")
+    x_data = data_frame[["T", "P", "TC", "SV"]].to_numpy()
+    y_data = data_frame["Idx"].to_numpy()
+
     reg = linear_model.LinearRegression()
     
 
