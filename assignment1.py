'Gradient Descent'. Calculated RMSE and R2, but need correct values.

assignment1.py
@@ -7,7 +7,6 @@ import numpy as np
 from numpy import random
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
 
 ''' develop the best predictive model based on the chemical engineering dataset'''
 
@@ -22,15 +21,13 @@ def main():
     x_data = data_frame[["T", "P", "TC", "SV"]].to_numpy()
     y_data = data_frame["Idx"].to_numpy()
 
-    # [80-20] training-testing %
-    testing_separation_index = math.floor(len(x_data) * 0.8)
-
-    least_squares_with_libraries(x_data, y_data, testing_separation_index)
-    stochastic_gradient_descent(x_data, y_data, testing_separation_index)
+    least_squares_with_libraries(x_data, y_data)
+    gradient_descent(x_data, y_data)
 
 
-def least_squares_with_libraries(x_data, y_data, testing_separation_index):
+def least_squares_with_libraries(x_data, y_data):
     # separate 80% of the data to training
+    testing_separation_index = math.floor(len(x_data) * 0.8)
     x_training = x_data[:testing_separation_index]
     x_testing = x_data[testing_separation_index:]
 
@@ -54,54 +51,49 @@ def least_squares_with_libraries(x_data, y_data, testing_separation_index):
 
 
 # still need to fix
-def stochastic_gradient_descent(x_data, y_data, testing_separation_index):
-    # separate 80% of the data to training
+def gradient_descent(x_data, y_data):
+    # separate 70% of the data to training
+    testing_separation_index = math.floor(len(x_data) * 0.8)
     x_training = x_data[:testing_separation_index]
     x_testing = x_data[testing_separation_index:]
 
     y_training = y_data[:testing_separation_index]
     y_testing = y_data[testing_separation_index:]
 
-    number_of_features = x_training.shape[1]  # 4 features
-
     # perform gradient descent method
-    w = np.ones(shape=number_of_features)  # shape [1. 1. 1. 1.]
+    reg = linear_model.LinearRegression()
+    reg.fit(x_training, y_training)
+
+    # what I tried before
+    # number_of_features = x_training.shape[1]  # 4 features
+    # np.ones(shape=number_of_features)  # [1. 1. 1. 1.]
+    w = reg.coef_  # [ 4.02221271e-02 -3.35092349e-03 -3.07098991e+01 -5.85740077e-02]
     b = 0
+    learning_rate = 0.01  # initial learning rate
+    num_iterations = 1000  # number of iterations or epoch
     total_samples = x_training.shape[0]  # N = total_samples
-    epochs = 100000  # number of iterations
-    learning_rate = 0.5
 
-    cost_list = []
-    epoch_list = []
+    for i in range(num_iterations + 1):
+        # Make predictions using dot product between weight(w) and x_testing samples
+        y_predicted = np.dot(w, x_testing.T) + b
 
-    for i in range(epochs + 1):
-        # separate 80% of the data to training
-        random_index = random.randint(0, total_samples - 1)
-        sample_x = x_training[random_index]
-        sample_y = y_training[random_index]
-
-        # Make predictions for y = mx + b
-        y_predicted = np.dot(w, sample_x.T) + b
-
-        # Calculate the gradients for weight(w) and bias(b)
-        m_grad = -(2 / total_samples) * (sample_x.T.dot(sample_y - y_predicted))
-        b_grad = -(2 / total_samples) * np.sum(sample_y - y_predicted)
+        # Calculate gradients for weight(w) and bias(b)
+        w_grad = -(2 / total_samples) * (x_training[i].T.dot(y_training[i] - y_predicted[i]))
+        b_grad = -(2 / total_samples) * np.sum(y_training[i] - y_predicted[i])
 
         # Update the current weight(w) and bias(b)
-        w = w - learning_rate * m_grad
+        w = w - learning_rate * w_grad
         b = b - learning_rate * b_grad
 
-        # Calculate the cost
-        cost = np.square(sample_y - y_predicted)
+        # Calculate the cost between y_training samples and y_predicted
+        cost = np.square(y_training[i] - y_predicted[i])
 
-        if i % 1000 == 0:
-            cost_list.append(cost)
-            epoch_list.append(i)
+    # Values might be incorrect
+    print("w: {}, b: {}, iteration: {}, cost: {}".format(w, b, i, cost))
 
-    # unsure if this value is correct, but set the iterations to 100,000 and got this.
-    print("m: {}, b: {}, epoch: {}, cost: {}".format(w, b, i, cost))
-    # print("The root mean squared error is", mean_squared_error(y_testing, y_predicted))
-    # print("The r squared score is", r2_score(y_training, y_predicted))
+    # Values are incorrect, still issues with calculations. (targets: 0 < RMSE < 1. and 0 < R2 < 1)
+    print("RMSE: ", mean_squared_error(y_testing, y_predicted))
+    print("R2: ", r2_score(y_testing, y_predicted))
 
 
 if __name__ == '__main__':